d828aeb20c0cecd7f28f150e122f8a51973a2649
hai <humg.ola@gmail.com>
2022-05-17 16:27:52 +0700

    up

---
+++ src/main/java/service/IUserDAO.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.User;
+
+import javax.jws.soap.SOAPBinding;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDAO {
+    public void insertUser(User user) throws SQLException;
+    public User selectUser(int id);
+    public List<User> selectAllUser();
+    List<User> findByName(String name);
+    public List<User> sortByName();
+    public boolean deleteUser(int id) throws SQLException;
+    public boolean updateUser(User user) throws SQLException;
+}
